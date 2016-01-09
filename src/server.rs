026260fbb94052d260fefefd271d36b774d555c0
Sinatra <root@stackin.money>
2016-01-08 21:53:29 -0500

    finalize

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,8 @@ extern crate user32;
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::ptr;
-use std::ffi::CString;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
 
 const GMEM_MOVEABLE: u32 = 0x0002;
 
@@ -21,23 +22,29 @@ fn main() {
 	}
 }
 
-fn set_data(foo: TcpStream) {
+fn set_data(foo: TcpStream) -> Result<i32, i32> {
 	let bar: Vec<_> = foo.chars().map(|c| c.unwrap()).collect();
-	let temp = bar.iter().cloned().collect::<String>();
-	let result =  CString::new(temp).unwrap();
+	let temp: String = bar.iter().cloned().collect::<String>();
+	let result: &std::ffi::OsStr = OsStr::new(&temp).as_ref();
     unsafe {
+		let buff: Vec<u16> = result.encode_wide().collect();
+        let len: usize = (buff.len()+1) * 2;
 		let open = user32::OpenClipboard(ptr::null_mut());
 		if open == 0 {
 			println!("couldnt open clipboard");
 		}
 		else {
-        	let h_Mem = kernel32::GlobalAlloc(GMEM_MOVEABLE, result.to_str().unwrap().len() as u32);
-			let data = kernel32::GlobalLock(h_Mem);
-			ptr::copy_nonoverlapping(result.as_ptr(), data as *mut i8, result.to_str().unwrap().len()); //look into
+        	let h_Mem = kernel32::GlobalAlloc(66, len as u32);
+			let data = kernel32::GlobalLock(h_Mem) as *mut u16;
+			let len: usize = (len - 1) / 2;
+            std::ptr::copy_nonoverlapping(buff.as_ptr(), data, len);
+            let len: isize = len as isize;
+            *data.offset(len) = 0;                                  //credits to clipboard-win for guidelines
 			user32::EmptyClipboard();
-			user32::SetClipboardData(13, data);
+			user32::SetClipboardData(13, h_Mem);
 			kernel32::GlobalUnlock(h_Mem);
 			user32::CloseClipboard();
 		}
 	}
+	return Ok(0);
 }
